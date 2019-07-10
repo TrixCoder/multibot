c505580f830386b9f8d79696eb6c64ca256c6116
BestCoderz <utk.bkkg@gmail.com>
2019-07-10 12:55:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -555,6 +555,7 @@ async def timer(ctx,timer=None,channel: discord.Channel=None, *, msg: str=None):
             output = mutetime/60
             r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
             embed=discord.Embed(title="Announcement", description="{}".format(msg), color = discord.Color((r << 16) + (g << 8) + b))
+            await client.send_message(channel, f"Ok ✅ I will announce your message in {output} minutes")
             await client.delete_message(ctx.message)
             await asyncio.sleep(mutetime)
             await client.send_message(channel, embed=embed)
@@ -575,6 +576,7 @@ async def timereveryone(ctx,timer=None,channel: discord.Channel=None, *, msg: st
             output = mutetime/60
             r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
             embed=discord.Embed(title="Announcement", description="{}".format(msg), color = discord.Color((r << 16) + (g << 8) + b))
+            await client.send_message(channel, f"Ok ✅ I will announce your message in {output} minutes")
             await client.delete_message(ctx.message)
             await asyncio.sleep(mutetime)
             await client.send_message(channel, '@everyone')
